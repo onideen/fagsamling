dd9f05433a420c6a7056dbc90d8e9f567ddbfc20
Vegar Engen <vegar.engen@visma.com>
2014-09-25 11:09:26 +0200

    added KONAMI code and some styling ting

--- app/assets/javascripts/javascripts/hello.js
+++ app/assets/javascripts/javascripts/hello.js
@@ -8,3 +8,22 @@ $(document).ready(function() {
         }
     });
 });
+
+
+var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
+
+$(document).keydown(function(e) {
+
+    kkeys.push( e.keyCode );
+
+    if ( kkeys.toString().indexOf( konami ) >= 0 ) {
+
+        $(document).unbind('keydown',arguments.callee);
+
+        alert("KONAMI");
+        // do something awesome
+        $("body").addClass("konami");
+
+    }
+
+});
