308f3e35a671ed8908f5ac7ec53e16bf1ae77c91
martejs <martesather@gmail.com>
2014-10-15 21:02:12 +0200

    ingenting fungerer...

--- app/models/Info.java
+++ app/models/Info.java
@@ -1,34 +1,47 @@
-package models;
-
-@Entity
-public class Info extends Model implements Comparable<Info>{
-
-
-    @Id
-    public long id;
-
-    private String name;
-
-    private String info;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getInfo() {
-            return info;
-        }
-
-    public void setInfo(String info) {
-            this.info = info;
-        }
-
-    public long getId(){
-    return id;
-    }
-
-}
+//package models;
+//import org.apache.commons.lang3.text.WordUtils;
+//import play.data.validation.Constraints;
+//import play.db.ebean.Model;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.Id;
+//import javax.persistence.OneToMany;
+//
+//import java.text.SimpleDateFormat;
+//import java.util.Collections;
+//import java.util.Date;
+//import java.util.List;
+//import java.util.Locale;
+//
+//@Entity
+//public class Info extends Model implements Comparable<Info>{
+//
+//	
+//    @Id
+//    public long id;
+//
+//    private String name;
+//
+//    private String info;
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getInfo() {
+//            return info;
+//        }
+//
+//    public void setInfo(String info) {
+//            this.info = info;
+//        }
+//
+//    public long getId(){
+//    return id;
+//    }
+//
+//}
